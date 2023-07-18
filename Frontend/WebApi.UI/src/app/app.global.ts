4b162bc7cee382c14511984af17d21089343c0f6
tarunpoddar <tarun.tarun05@gmail.com>
2023-07-18 21:58:36 +0530

    Added the password key and value in the header in each http request while calling the Web Api.

--- Frontend/WebApi.UI/src/app/app.global.ts
+++ Frontend/WebApi.UI/src/app/app.global.ts
@@ -1,2 +1,7 @@
+import { HttpHeaders } from '@angular/common/http';
+
 export const baseUrl: string="https://localhost:7162/";
 
+export const myHeaders: HttpHeaders = new HttpHeaders({
+    passwordKey: 'passwordKey123456789',
+  });
--- Frontend/WebApi.UI/src/app/services/employees.service.ts
+++ Frontend/WebApi.UI/src/app/services/employees.service.ts
@@ -1,35 +1,52 @@
-import { HttpClient } from '@angular/common/http';
+import { HttpClient, HttpHeaders } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 import { Observable } from 'rxjs';
-import { baseUrl } from '../app.global';
+import { baseUrl, myHeaders } from '../app.global';
 import { Employee } from '../models/employee.model';
 
 @Injectable({
-  providedIn: 'root'
+  providedIn: 'root',
 })
-
 export class EmployeesService {
-
   baseApiUrl: string = baseUrl;
+
   constructor(private http: HttpClient) { }
 
   getAllEmployees(): Observable<Employee[]> {
-    return this.http.get<Employee[]>(this.baseApiUrl + 'api/employees');
+    return this.http.get<Employee[]>(this.baseApiUrl + 'api/employees', {
+      headers: myHeaders,
+    });
   }
 
   getEmployee(id: string): Observable<Employee> {
-    return this.http.get<Employee>(this.baseApiUrl + 'api/employees/' + id);
+    return this.http.get<Employee>(this.baseApiUrl + 'api/employees/' + id, {
+      headers: myHeaders,
+    });
   }
 
   addEmployee(employee: Employee): Observable<Employee> {
-    return this.http.post<Employee>(this.baseApiUrl + 'api/employees', employee);
+    return this.http.post<Employee>(
+      this.baseApiUrl + 'api/employees',
+      employee,
+      {
+        headers: myHeaders,
+      }
+    );
   }
 
   updateEmployee(employee: Employee): Observable<Employee> {
-    return this.http.put<Employee>(this.baseApiUrl + 'api/employees/' + employee.id, employee);
+    return this.http.put<Employee>(
+      this.baseApiUrl + 'api/employees/' + employee.id,
+      employee,
+      {
+        headers: myHeaders,
+      }
+    );
   }
 
   deleteEmployee(id: string): Observable<Employee> {
-    return this.http.delete<Employee>(this.baseApiUrl + 'api/employees/' + id);
+    return this.http.delete<Employee>(this.baseApiUrl + 'api/employees/' + id, {
+      headers: myHeaders,
+    });
   }
 }
